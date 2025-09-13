29f9dbedf9928965b8741729c3ca8477463ea2b2
DoanCongPho <congpho115@gmail.com>
2025-09-13 23:24:20 +0700

    fix

--- src/main/java/com/example/finalproject/journey/ui/QuizListScreen.kt
+++ src/main/java/com/example/finalproject/journey/ui/QuizListScreen.kt
@@ -66,7 +66,8 @@ fun QuizListScreen(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(WindowInsets.statusBars.asPaddingValues())
+                    .padding(horizontal = 24.dp),
             ) {
                 IconButton(
                     onClick = onBack,
